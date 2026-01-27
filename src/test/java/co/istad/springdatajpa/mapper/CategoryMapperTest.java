11e4113b09a66283d3da42026712e738c6b4d9d0
Sattya Piseth <pisethsattya33@gmail.com>
2026-01-27 13:53:00 +0700

    test: inject ProductMapper into CategoryMapperImpl for unit tests

--- src/test/java/co/istad/springdatajpa/mapper/CategoryMapperTest.java
+++ src/test/java/co/istad/springdatajpa/mapper/CategoryMapperTest.java
@@ -5,15 +5,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import co.istad.springdatajpa.dto.response.CategoryResponse;
 import co.istad.springdatajpa.entity.Category;
 import co.istad.springdatajpa.entity.Product;
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 class CategoryMapperTest {
 
     private final CategoryMapper mapper = Mappers.getMapper(CategoryMapper.class);
+    private final ProductMapper productMapper = Mappers.getMapper(ProductMapper.class);
+
+    @BeforeEach
+    void wireMappers() throws Exception {
+        if (mapper instanceof CategoryMapperImpl impl) {
+            Field field = CategoryMapperImpl.class.getDeclaredField("productMapper");
+            field.setAccessible(true);
+            field.set(impl, productMapper);
+        }
+    }
 
     @Test
     void toResponse_mapsProductSummaries() {
